903f0742c7278f72fb76e86678ada39d68e6c504
Ardash <ardache@hotmail.com>
2020-08-17 13:33:22 -0500

    cert

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,12 @@ router.get('/single-portfolio2.hbs', (req, res, next) => {
   res.render('single-portfolio2');
 });
 
-
-router.get('/.well-known/acme-challenge/:id', function(req, res, next) {
-  res.send(req.params.id+'.'+ WEBZEITEN_CERTBOT_ID);
+router.get(`/.well-known/acme-challenge/PJVXP9Gg7ZDMlSxhzkLA4BPkWn1Fuq-G88qDbLpGdew`, (req, res, next) => {
+  //res.render('single-portfolio2');
+   //res.send(req.params.id+'.'+ WEBZEITEN_CERTBOT_ID);
+  res.send(`PJVXP9Gg7ZDMlSxhzkLA4BPkWn1Fuq-G88qDbLpGdew.l0vgkAiyrdxRZrqgzQxVe_8fVtkWMXpluK5nmNyJxhA`);
 });
+
 
 // router.post('/php/sendMail.php', (req, res, next) => {
   
